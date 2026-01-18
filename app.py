bad88eb0462e4f42054cdebe6c246ef908d3c4b0
vijeshwar <vijeshwar@student.tce.edu>
2026-01-18 22:06:47 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-binary = input("Enter a binary number: ")
 
-decimal = 0
-power = 0
 
-for digit in reversed(binary):
-    decimal += int(digit) * (2 ** power)
-    power += 1
-
-print("Decimal value:", decimal)
+print("Decimal value:")
